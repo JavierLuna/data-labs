d41d5cb3e99eb2d155a230375c399129530cfe92
Alfon <alfonso_aguado@hotmail.es>
2019-05-25 11:48:50 +0200

    Allmost Done

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -12,24 +12,28 @@ ORDER BY authors.au_id ASC;
 -- Challenge nº2
 -- Now we need to count how many titles each author has publish with each Publisher.
 
-SELECT authors.au_id AS "Authors ID", authors.au_lname AS "Last Name", authors.au_fname AS "First Name", publishers.pub_name AS "Publisher", COUNT(titles.title) AS "Title Count" 
+SELECT authors.au_id AS "Authors ID", authors.au_lname AS "Last Name", authors.au_fname AS "First Name", publishers.pub_name AS "Publisher", 
+COUNT(titles.title_id) AS "Title Count" 
 FROM authors
 INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
 INNER JOIN titles ON titleauthor.title_id = titles.title_id
 INNER JOIN publishers ON titles.pub_id = publishers.pub_id
 
-GROUP BY author.au_id,authors.au_lname,authors.au_fname, publishers.pub_name;
+-- We now group by the columms but the COUNT, and we ordered by the COUNT in Desc order.
+GROUP BY authors.au_id, authors.au_lname, authors.au_fname,publishers.pub_name
+ORDER BY COUNT(titles.title_id) DESC;
 
 -- Challenge nº3
 -- Now the query should give as the authors with the highest number of titles.
 
-SELECT authors.au_id AS "Authors ID", authors.au_lname AS "Last Name", authors.au_fname AS "First Name", publishers.pub_name AS "Publisher", SUM(titles.title) AS "Total"
+SELECT authors.au_id AS "Authors ID", authors.au_lname AS "Last Name", authors.au_fname AS "First Name", publishers.pub_name AS "Publisher", 
+COUNT(titles.title_id), COUNT(titles.pub_id) 
 FROM authors
 INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
 INNER JOIN titles ON titleauthor.title_id = titles.title_id
 INNER JOIN publishers ON titles.pub_id = publishers.pub_id
 
-GROUP BY author.au_id,authors.au_lname,authors.au_fname, publishers.pub_name;
-ORDER BY SUM(titles.title
-DESC;
+-- 
+GROUP BY authors.au_id, authors.au_lname, authors.au_fname,publishers.pub_name
+ORDER BY COUNT(titles.title_id) DESC;
 
