0f5f772183f816c056d96be5f79380457cee5ecc
Alfon <alfonso_aguado@hotmail.es>
2019-05-25 10:48:11 +0200

    With some errors but the logic is there

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -12,22 +12,24 @@ ORDER BY authors.au_id ASC;
 -- Challenge nº2
 -- Now we need to count how many titles each author has publish with each Publisher.
 
-SELECT authors.au_id AS "Authors ID", authors.au_lname AS "Last Name", authors.au_lname AS "First Name", publishers.pub_name AS "Publisher", COUNT(titles.title) AS "Title Count" 
+SELECT authors.au_id AS "Authors ID", authors.au_lname AS "Last Name", authors.au_fname AS "First Name", publishers.pub_name AS "Publisher", COUNT(titles.title) AS "Title Count" 
 FROM authors
 INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
 INNER JOIN titles ON titleauthor.title_id = titles.title_id
 INNER JOIN publishers ON titles.pub_id = publishers.pub_id
 
-GROUP BY titles.title;
+GROUP BY author.au_id,authors.au_lname,authors.au_fname, publishers.pub_name;
 
 -- Challenge nº3
 -- Now the query should give as the authors with the highest number of titles.
 
-SELECT authors.au_id AS "Authors ID", authors.au_lname AS "Last Name", authors.au_lname AS "First Name", titles.title AS "Title", publishers.pub_name AS "Publisher", SUM(titles.title) AS "Total"
+SELECT authors.au_id AS "Authors ID", authors.au_lname AS "Last Name", authors.au_fname AS "First Name", publishers.pub_name AS "Publisher", SUM(titles.title) AS "Total"
 FROM authors
 INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
 INNER JOIN titles ON titleauthor.title_id = titles.title_id
 INNER JOIN publishers ON titles.pub_id = publishers.pub_id
 
-ORDER BY authors.au_id ASC;
+GROUP BY author.au_id,authors.au_lname,authors.au_fname, publishers.pub_name;
+ORDER BY SUM(titles.title
+DESC;
 
