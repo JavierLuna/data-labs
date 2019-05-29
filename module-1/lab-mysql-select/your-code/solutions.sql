3cc838809d9bc96f759a072358a11eff45f9b004
ElenaCerezoSwing <elenacerezo83@gmail.com>
2019-05-29 08:08:46 +0200

    adds last comments

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -22,7 +22,7 @@ INNER join publishers ON titles.pub_id = publishers.pub_id
 ORDER BY authors.au_lname;
 
 -- CHALENGE 2
--- "¿Cuántos títulos de cada autor han sido publicados por cada editorial?
+-- "¿Cuántos títulos de cada autor o autora han sido publicados por cada editorial?
 -- Para ello, podemos partir de la selección anterior porque la única diferencia existente es
 -- que en lugar de dar el nombre de los títulos vamos a dar el número de título de cada autor por editorial.
 -- Ahora vamos a seleccionar la columnsa de title_id para evitar posibles duplicados en los nombres de los títulos
@@ -30,6 +30,7 @@ ORDER BY authors.au_lname;
 -- títulos existen por la combinación de autor o autora, su nombre, su apellido y una editorial concreta. Así que lo
 -- agrupamos con GROUP BY y lo ordenamos por COUNT(titles.title_id)
 -- NOTA: es muy importante que si lo que vamos a dar es el número de títulos, hagamos un count()
+-- NOTA 2: si no lo ordenamos también se ejecuta la consulta
 SELECT authors.au_id AS "Author_ID", authors.au_lname AS "Last_Name", 
 authors.au_fname AS "First_Name", publishers.pub_name AS "Publisher",
 count(titles.title_id) AS "Title Count"
