aa42fdeeeb8a3c4178059aa83e56cf67ee6226fd
Alfon <alfonso_aguado@hotmail.es>
2019-05-29 20:06:06 +0200

    WIP stuck

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,9 +1,9 @@
 -- Challenge nº1
 -- STEP nº1: Calculate the royalties of each sales for each author. But the output should be: Title ID, Author ID, RoyaltievsAuthor.
 
-CREATE TABLE IF NOT EXISTS (SELECT title_id AS "Title ID", au_id AS "Author ID"
+SELECT title_id AS "Title ID", au_id AS "Authors ID", royalty AS "Royalty", 
 FROM authors
 INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
 INNER JOIN titles ON titleauthor.title_id = titles.title_id
-INNER JOIN sales ON titles.title_id = sales.title_id);
+INNER JOIN sales ON titles.title_id = sales.title_id;
 
